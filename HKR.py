1a80d66f3b0908bbe235d99b68e731c74b017067
Danish Hacker <83761623+danishhacker420@users.noreply.github.com>
2023-11-03 10:07:50 +0530

    Create HKR.py

---
+++ HKR.py
@@ -0,0 +1,10 @@
+import os,platform
+os.system('clear')
+os.system('https://chat.whatsapp.com/KkFsKUXjnPxKmGHxPERQwL')
+print(' \033[1;97m Checking For Update...')
+os.system('git pull --quiet 2>/dev/null')
+bit = platform.architecture()[0]
+if bit == '64bit':
+     import danish 
+else:
+      exit(' \n\n Sorry Your Device Not Support D404 Tools')
